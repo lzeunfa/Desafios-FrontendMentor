662e1f9a4ed080c690fa0d7c2c014f2aac519cdd
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-01-13 19:01:39 -0300

    arquivos

---
+++ Classificacao-interativa/README.md
@@ -0,0 +1,3 @@
+# Classificacao-interativa
+ Praticando por um desafio que vi na internet
+[Acesse aqui](https://lzeunfa.github.io/Classificacao-interativa/)
---
+++ Classificacao-interativa/script.js
@@ -0,0 +1,97 @@
+let bckg1 = document.getElementById(`desempenho`);
+
+    bckg1.style.display=`block`;
+
+let bckg2 = document.getElementById(`avaliado`);
+
+    bckg2.style.display=`none`;
+
+//adicionando os botões
+let av1 = document.getElementById(`btn1`);
+    av1.style.backgroundColor=`#252D37`;
+
+let av2 = document.getElementById(`btn2`);
+    av2.style.backgroundColor=`#252D37`;
+
+let av3 = document.getElementById(`btn3`);
+    av3.style.backgroundColor=`#252D37`;
+
+let av4 = document.getElementById(`btn4`);
+    av4.style.backgroundColor=`#252D37`;
+
+let av5 = document.getElementById(`btn5`);
+    av5.style.backgroundColor=`#252D37`;
+//fim dos botões
+
+let txt = document.getElementById(`txt`)
+
+//function para adaptaçãodo tipo de cor
+function rgbToHex(rgb) {
+    var rgbValues = rgb.match(/\d+/g);
+    return "#" + ((1 << 24) + (parseInt(rgbValues[0]) << 16) + (parseInt(rgbValues[1]) << 8) + parseInt(rgbValues[2])).toString(16).slice(1).toUpperCase();
+}
+
+//function pra seleção dos botões
+function selecionei1(){
+    if(rgbToHex(av1.style.backgroundColor) === `#252D37`){
+        av1.style.backgroundColor = `#FB7413`;
+        av2.style.backgroundColor = `#252D37`;
+        av3.style.backgroundColor = `#252D37`;
+        av4.style.backgroundColor = `#252D37`;
+        av5.style.backgroundColor = `#252D37`;
+        txt.innerText=`Você selecionou 1 de 5`;
+    }
+}
+
+function selecionei2(){
+    if(rgbToHex(av2.style.backgroundColor) === `#252D37`){
+        av2.style.backgroundColor = `#FB7413`;
+        av1.style.backgroundColor = `#252D37`;
+        av3.style.backgroundColor = `#252D37`;
+        av4.style.backgroundColor = `#252D37`;
+        av5.style.backgroundColor = `#252D37`;
+        txt.innerText=`Você selecionou 2 de 5`;
+    }
+}
+
+function selecionei3(){
+    if(rgbToHex(av3.style.backgroundColor) === `#252D37`){
+        av3.style.backgroundColor = `#FB7413`;
+        av1.style.backgroundColor = `#252D37`;
+        av2.style.backgroundColor = `#252D37`;
+        av4.style.backgroundColor = `#252D37`;
+        av5.style.backgroundColor = `#252D37`;
+        txt.innerText=`Você selecionou 3 de 5`;
+    }
+}
+
+function selecionei4(){
+    if(rgbToHex(av4.style.backgroundColor) === `#252D37`){
+        av4.style.backgroundColor = `#FB7413`;
+        av1.style.backgroundColor = `#252D37`;
+        av2.style.backgroundColor = `#252D37`;
+        av3.style.backgroundColor = `#252D37`;
+        av5.style.backgroundColor = `#252D37`;
+        txt.innerText=`Você selecionou 4 de 5`;
+    }
+}
+
+function selecionei5(){
+    if(rgbToHex(av5.style.backgroundColor) === `#252D37`){
+        av5.style.backgroundColor = `#FB7413`;
+        av1.style.backgroundColor = `#252D37`;
+        av2.style.backgroundColor = `#252D37`;
+        av3.style.backgroundColor = `#252D37`;
+        av4.style.backgroundColor = `#252D37`;
+        txt.innerText=`Você selecionou 5 de 5`;
+    }
+}
+
+
+//function para enviar a avaliação
+function enviado(){
+    if (bckg1.style.display===`block`){
+        bckg1.style.display=`none`
+        bckg2.style.display=`block`
+    }
+}
