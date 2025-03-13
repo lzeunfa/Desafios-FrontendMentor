9d748540b8c309824fe5847eed72723b4cc66efc
Eliseu <153873892+lzeunfa@users.noreply.github.com>
2025-03-13 20:24:59 -0300

    realizei alterações para gerar interações de compra mas nada demais, após aprofundar melhor meu conhecimentos e js eu finalizo este projeto

--- Product-List/list.js
+++ Product-List/list.js
@@ -1,20 +1,31 @@
+//botão
 let buttonWaff = document.getElementById('buyWaff');
 buttonWaff.style.backgroundColor = 'rgb(252, 249, 247)';
 buttonWaff.style.display=`block`;
 
 let cartwimg = document.getElementById(`cartw`);
 
+//imagem da comida
+let imgW = document.getElementById(`imgW`);
+    imgW.style.transition = `ease 0.05s`;
+
 //abaixo é o outro botão
 
 let buttonAdd = document.getElementById(`add`);
 buttonAdd.style.display=`none`;
 
+//para receber a mudança de número
+let quantidade = document.getElementById(`quantidade`);
+
 function buying() {//Mudança de icons dos botões de compra
 
     if (buttonWaff.style.display === 'block') {
         buttonAdd.style.display=`block`;
+        imgW.style.border = `3px solid #CD5E43`;
 
         buttonWaff.style.display = 'none';
+
+        quantidade.innerText = `1`;
 
     } else {
         buttonWaff.style.display = 'block';
@@ -25,9 +36,24 @@ function buying() {//Mudança de icons dos botões de compra
 
 //Parte para adição dos produtos e preços
 function increment(){
-
+    
 }
 
-function deecrement(){
+function decrement(){
+    if (buttonWaff.style.display === 'block') {
+        buttonAdd.style.display=`block`;
 
+        imgW.style.border = `3px solid #CD5E43`;
+
+        buttonWaff.style.display = 'none';
+
+    } else {
+        buttonWaff.style.display = 'block';
+
+        buttonAdd.style.display=`none`;
+
+        imgW.style.border = `none`;
+
+        quantidade.innerText = `0`;
+    }
 }
