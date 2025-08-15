90c4927de197952ff1d795e02a409b020a03fdf1
Eliseu <eliseu.nfa@gmail.com>
2025-08-15 12:07:21 -0300

    alterações de classes no btn, adicionei span pro conteudo do btn ser alterado separadamente e adicionei teste no js para ver se estava funcionando o onclick separadamente

--- Product-List/list.js
+++ Product-List/list.js
@@ -1,3 +1,6 @@
+let btns = document.getElementsByClassName('btns');
+
 function adicionar(identificador,nome,valor,qtde){
-    
+    btns[identificador].style.backgroundColor = 'red';
+    return;
 }
