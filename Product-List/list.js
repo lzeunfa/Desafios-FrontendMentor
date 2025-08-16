1c5b3deefdcbebdeaca10fce3227a4e71a801eab
Eliseu <eliseu.nfa@gmail.com>
2025-08-16 12:08:37 -0300

    adicionei as classes dos btns e seus spans para poderem ser modificados

--- Product-List/list.js
+++ Product-List/list.js
@@ -1,6 +1,23 @@
+//recebe todos os button
 let btns = document.getElementsByClassName('btns');
 
-function adicionar(identificador,nome,valor,qtde){
-    btns[identificador].style.backgroundColor = 'red';
+//span do botão quando não está selecionado
+let textBtnsVazio = document.getElementsByClassName("textBtnsVazio");
+for(let i = 0; i<textBtnsVazio.length;i++){
+    textBtnsVazio[i].style.display = "block";
+}
+
+//span do botão quando foi selecionado
+let textBtnsClicado = document.getElementsByClassName('textBtnsClicado');
+for(let i = 0; i<textBtnsClicado.length;i++){
+    textBtnsClicado[i].style.display = "none";
+}
+
+//função executada ao clicar o btn
+function selecionado(identificador,nome,valor,qtde){
+    textBtnsVazio[identificador].style.display = "none";   
+
+    textBtnsClicado[identificador].style.display = "block";
+
     return;
 }
